cd6065ef9000f6665fdd0ac81bda3b85543155ec
sgvanvleet <svanvlee@purdue.edu>
2017-09-27 21:24:18 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var app = express();
 var bodyParse = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
 app.use(bodyParse.urlencoded({extended : true}));
 app.use(bodyParse.json());
+var url = 'mongodb://cconcep:Republica1!@ds141454.mlab.com:41454/node_tutorial';
+
+MongoClient.connect(url,function(err,db) {
+  assert.equal(null,err);
+  console.log("Connected correctly to server.");
+  db.close();
+})
+
+var server = app.listen(8081, function() {    //when you first start the server?
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port);
+})
 
 app.post('/sendLogin', function (req,res) {
   console.log("Received a POST req for /sendLogin");
@@ -13,6 +29,7 @@ app.post('/sendLogin', function (req,res) {
   var json = req.body;
   console.log("The JSON string is: " + req.body);
 
+  //check formatting?
   if(!json.hasOwnProperty("username")) {
     console.log("Whoa, you aint got no name, dude");
   }
@@ -20,26 +37,12 @@ app.post('/sendLogin', function (req,res) {
   var user = json['username'];
   var pass = json['password'];
 
-  //pass to db 'user', get JSON string
-  //parse JSON string, get the pass
-  //if (pass ==  passFromDB)
-  //  make new token
-  //  save token to DB
-  //  redirect client to next page
-  //  give client toekn
-  //else
-  //  tell client that login failed
-  //  refresh page
-  //
+  MongoClient.connect(url,function(err,db) {
+    assert.equal(null,err);
+    console.log("Connected correctly to server.");
 
+    //db.collection('users').insertOne(json);
 
-
-})
-
-
-var server = app.listen(8081, function() {    //when you first start the server?
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Example app listening at http://%s:%s", host, port);
+    db.close();
+  })
 })
