1937c57e5428575cd964e7b98f808bc313339b0c
tkotekar <tejaswi.kotekar@gmail.com>
2017-11-02 16:48:36 -0400

    Completed To-Do List Backend

--- practice_server/src/routes/ToDoListRouter.js
+++ practice_server/src/routes/ToDoListRouter.js
@@ -8,23 +8,9 @@ var url = 'mongodb://cconcep:Republica1!@ds141454.mlab.com:41454/node_tutorial'
 var ToDoListRouter = express.Router();
 
 // Require UserInfo model in our routes module
-var ToDoList = require('../models/ToDoList');
+var ToDoList = require('../models/todo');
 
-
-// Defined get data(index or listing) route
-ToDoListRouter.route('/').get(function (req, res) {
-  ToDoList.find(function (err, itms){
-    if(err){
-      console.log(err);
-    }
-    else {
-      res.json(itms);
-    }
-  });
-});
-
-
-// Defined store route
+// Defined route
 ToDoListRouter.route('/initialize').post(function (req, res) {
   var info = new ToDoList(req.body);
       info.save()
@@ -36,25 +22,20 @@ ToDoListRouter.route('/initialize').post(function (req, res) {
     });
 });
 
-//Retreives and sends ToDo List of specific user
-ToDoListRouter.route('/get-user-list').post(function (req, res) {
-  var info = req.body;
-  var user = info['userID'];
 
-  ToDoList.find({userID: user}, function (err, itms){
+// Defined get data(index or listing) route
+ToDoListRouter.route('/').get(function (req, res) {
+  ToDoList.find(function (err, tasks){
     if(err){
       console.log(err);
     }
     else {
-      res.json(itms);
+      res.json(tasks);
     }
   });
-})
+});
 
-//deletes a todo from the list
-ToDoListRouter.route('/')
-
-// Revise password of specific UserID
+//find task and save and update
 ToDoListRouter.route('/add-entry').post(function (req, res) {
   var info = req.body;
   var user = info['userID'];
@@ -66,10 +47,70 @@ ToDoListRouter.route('/add-entry').post(function (req, res) {
       console.log(err);
     }
     else {
+      console.log(itms);
       res.json(itms);
     }
   });
 })
+
+//deletes task
+ToDoListRouter.route('/delete-entry').post(function(req, res){
+  var info = req.body;
+  var user = info['userID'];
+  var items = info['items'];
+
+  var query = {userID: user};
+  ToDoList.findOneAndUpdate(query, {items:items}, function(err,itms){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log(itms);
+      res.json(itms);
+    }
+  });
+})
+
+//Save a task
+/*ToDoListRouter.post('/saveTask',function(req,res,next){
+  var taskSaved = req.body;
+  var task = taskSaved['entries'];
+  //var user = info['userID'];
+  //var items = info['items'];
+  // if(!task.title|| (task.isDone + '')){
+  //   res.status(400);
+  //   res.json({
+  //     "error": "Bad Data"
+  //   });
+  // }
+  //else{
+    ToDoList.save(function(err,taskSaved, numAffected){
+      if(err){
+        console.log(err);
+        res.send(err);
+      }
+      console.log(task);
+      res.json(task);
+    })
+    // .then(result => console.log(result))
+  //}
+});*/
+
+/*//Get all tasks
+ToDoListRouter.route('/get-user-list').post(function (req, res) {
+  var info = req.body;
+  var user = info['userID'];
+
+  ToDoList.find({userID: user}, function (err, itms){
+    if(err){
+      console.log(err);
+    }
+    else {
+      iems.
+      console.log(itms);
+    }
+  });
+})*/
 
 
 module.exports = ToDoListRouter;
